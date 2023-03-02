7ed8b8f6cb0385251c4268ef8811cef3410b4e21
Nazrul Islam Nayan <43903460+nayanchamp7@users.noreply.github.com>
2023-03-02 19:58:59 +0600

    initialize the plugin base files and main class

---
+++ includes/class-spx-loader.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * SPX Main Class
+ *
+ * @package  SPX_Loader
+ */
+
+defined('ABSPATH') || exit;
+
+class SPX_Loader
+{
+    protected static $_instance = null;
+
+    // constructor
+    function __construct()
+    {
+        $this->includes();
+        $this->hooks();
+        do_action('spx_loaded', $this);
+    }
+
+    // version
+    function version()
+    {
+        return esc_attr(SPX_VERSION);
+    }
+
+    // define
+    protected function define($name, $value)
+    {
+        if (!defined($name)) {
+            define($name, $value);
+        }
+    }
+
+    // instance
+    static function instance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+    // includes files
+    function includes()
+    {
+
+    }
+
+    // action and filter hooks
+    function hooks()
+    {
+        // init
+        add_action('init', [$this, 'init']);
+    }
+
+    // plugin init
+    function init()
+    {
+    }
+
+    public function language()
+    {
+        load_plugin_textdomain('spacex-rocket', false, plugin_basename(dirname(SPX_PLUGIN_FILE)) . '/languages');
+    }
+
+    public function basename()
+    {
+        return basename(dirname(SPX_PLUGIN_FILE));
+    }
+
+    public function plugin_basename()
+    {
+        return plugin_basename(SPX_PLUGIN_FILE);
+    }
+
+    public function plugin_dirname()
+    {
+        return dirname(plugin_basename(SPX_PLUGIN_FILE));
+    }
+
+    public function plugin_path()
+    {
+        return untrailingslashit(plugin_dir_path(SPX_PLUGIN_FILE));
+    }
+
+    public function plugin_url()
+    {
+        return untrailingslashit(plugins_url('/', SPX_PLUGIN_FILE));
+    }
+
+    function include_path($file = '')
+    {
+        return untrailingslashit(plugin_dir_path(SPX_PLUGIN_FILE) . 'includes') . $file;
+    }
+}
---
+++ spacex-rocket.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name:       SpaceX Rocket
+ * Description:       A simple gutenberg block plugin to show spacex rockets.
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           1.0.0
+ * Author:            Nazrul Islam Nayan
+ * Author URI:        https://profiles.wordpress.org/nayanchamp7/
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       spacex-rocket
+ *
+ * @package           SpaceX
+ */
+
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'SPX_VERSION' ) ) {
+    define( 'SPX_VERSION', '1.0.0' );
+}
+
+if ( ! defined( 'SPX_PLUGIN_FILE' ) ) {
+    define( 'SPX_PLUGIN_FILE', __FILE__ );
+}
+
+// Include the main class.
+if ( ! class_exists( 'SPX_Loader', false ) ) {
+    require_once dirname( __FILE__ ) . '/includes/class-spx-loader.php';
+}
+
+/**
+ * Returns the main instance.
+ */
+function spx_loader_callback() { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName.FunctionNameInvalid
+    return SPX_Loader::instance();
+}
+
+add_action( 'plugins_loaded', 'spx_loader_callback' );
